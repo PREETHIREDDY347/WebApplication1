6e7cf6346ee905d245ca877dbe8acfdbe0064854
PREETHIREDDY347 <preethireddy7511@gmail.com>
2022-01-29 10:42:16 +0530

    Add project files.

---
+++ WebApplication1/Controllers/TutorialController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TutorialEntry()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Tutorial
+    {
+        public int TutorialId { get; set; }
+        public string TutorialName { get; set; }
+        public string TutorialDesc { get; set; }
+        public int Published { get; set; }
+        public int Fees { get; set; }
+    }
+}
